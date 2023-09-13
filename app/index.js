83bc4697f16edad790e42196abedac3e0fa96ae8
corne97 <c0rn31997@hotmail.com>
2023-09-13 12:15:57 +0200

    added delete button with styling(Function doesnt work yet)

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const { ipcRenderer, mainWindow } = require('electron');
 const fs = require("fs");
 
+
 // Add a click event listener to the button
 const openBtn = document.getElementById('openNewWindowButton')
 openBtn.addEventListener('click', function (event)
@@ -9,12 +10,13 @@ openBtn.addEventListener('click', function (event)
   ipcRenderer.send('open-newWindow');
 });
 
+
 // Add a click event listener to the submit button
 const savebtn = document.getElementById('submit')
 savebtn.addEventListener('click', function (event)
 {
   saveData();
-  
+
 });
 
 
@@ -51,26 +53,75 @@ function saveData()
       console.log(err);
       return;
     }
+
     console.log("text has been saved to test.txt")
     console.log(listarr)
 
     renderList();
   }
   )
+
 }
+
+
+
+
+function deleteData(li) 
+{
+
+  // get the textContext from de html list element
+  const txt = li.textContent;
+  // find the index of the txt in te arrlist
+  const index = arrlist.indexOf(txt);
+
+  // if the index === -1 it means that nothing is found
+  if (index === -1)
+  {
+    // so lets show an error
+    alert("Could not find list item");
+    // and return out of the function
+    return;
+  }
+
+  // if the index !== -1 we can remove it
+  arrlist.splice(index, 1); // remove from arrlist at index, but only remove 1
+
+  // and as last, we remove the html list element
+  li.remove(li);
+
+  // remove item element
+  document.getElementById("list").innerHTML = "";
+}
+
+
+
 
 // Render the list after data is saved to the file
 function renderList()
 {
-  let list = document.getElementById('list');
 
+  let list = document.getElementById('list');
+  //clear list element to reduce duplicates
   document.getElementById('list').innerHTML = "";
+
 
   listarr.forEach((item) =>
   {
     let li = document.createElement("li");
+    let btn = document.createElement("button");
+    btn.id="delete";
     li.innerText = item;
     list.appendChild(li);
+    li.appendChild(btn);
+
+    btn.innerHTML ='<img src="../images/trashbin.svg"/>'
+    
+    btn.addEventListener("click", function ()
+    {
+      deleteData(li);
+      console.log("Button is clicked!");
+    });
+      
   });
 }
 
